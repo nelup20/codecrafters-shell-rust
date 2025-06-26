8906c6815b17e82e57a5b9880b52726080f1213a
Nelu Platonov <nelu.platonov@gmail.com>
2025-06-27 01:13:32 +0300

    Add TODO for parse_args

--- src/util/args.rs
+++ src/util/args.rs
@@ -1,6 +1,11 @@
 pub fn parse_args(args: &str) -> Vec<String> {
     let args = args.trim().replace("''", "").replace("\"\"", "");
-    
+
+    // TODO: doesn't actually handle escaping properly (despite passing the stage)
+    // ex:
+    // input    --> echo "some item" \"another item\"
+    // expected --> some item "another item"
+    // actual   --> "some item" "another item"
     if contains_unescaped_double_quotes(&args) {
         split_by_separator(args, "\"")
     } else if contains_unescaped_single_quotes(&args) {
