80c43c5891ec16bc120fbb31dc5ce64d0d8b54fc
Nelu Platonov <nelu.platonov@gmail.com>
2025-06-26 18:49:37 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,27 @@ fn main() {
 
         let input = input.trim();
 
-        if input.starts_with("exit") {
-            let exit_code = input.split_once(" ").unwrap().1.parse().unwrap();
-            std::process::exit(exit_code);
-        } else {
-            println!("{}: command not found", input.trim());
+        match input {
+            cmd if cmd.starts_with("echo") => handle_echo(cmd),
+            cmd if cmd.starts_with("exit") => handle_exit(cmd),
+            cmd => handle_unknown(cmd)
         }
     }
+}
+
+#[inline(always)]
+fn handle_exit(cmd: &str) {
+    let exit_code = cmd.split_once(" ").unwrap().1.parse().unwrap();
+    std::process::exit(exit_code);
+}
+
+#[inline(always)]
+fn handle_echo(cmd: &str) {
+    let (_, to_echo) = cmd.split_once(" ").unwrap();
+    println!("{to_echo}");
+}
+
+#[inline(always)]
+fn handle_unknown(cmd: &str) {
+    println!("{cmd}: command not found");
 }
