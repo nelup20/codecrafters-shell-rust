6104bb40a0d338c1490f3384023c21c8ad63ac7e
Nelu Platonov <nelu.platonov@gmail.com>
2025-06-27 01:10:48 +0300

    codecrafters submit [skip ci]

--- src/util/args.rs
+++ src/util/args.rs
@@ -1,15 +1,32 @@
-
 pub fn parse_args(args: &str) -> Vec<String> {
     let args = args.trim().replace("''", "").replace("\"\"", "");
-
-    let separator = if args.contains("\""){
-        "\""
-    } else if args.contains("'") {
-        "'"
+    
+    if contains_unescaped_double_quotes(&args) {
+        split_by_separator(args, "\"")
+    } else if contains_unescaped_single_quotes(&args) {
+        split_by_separator(args, "'")
+    } else if !args.contains("\\") {
+        split_by_separator(args, " ")
     } else {
-        " "
-    };
+        return args
+            .replace("\\", "")
+            .split(" ")
+            .map(|arg| arg.to_owned())
+            .collect();
+    }
+}
 
+#[inline(always)]
+fn contains_unescaped_single_quotes(args: &str) -> bool {
+    args.contains("'") && !args.contains("\\\'")
+}
+
+#[inline(always)]
+fn contains_unescaped_double_quotes(args: &str) -> bool {
+    args.contains("\"") && !args.contains("\\\"")
+}
+
+fn split_by_separator(args: String, separator: &str) -> Vec<String> {
     args.split(separator)
         .filter(|arg| !arg.trim().is_empty())
         .map(|arg| arg.trim().to_owned())
