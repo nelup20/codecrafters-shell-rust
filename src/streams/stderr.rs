b7cbdcd8d5bb2190f7145fbf06a3e99d765ea528
Nelu Platonov <nelu.platonov@gmail.com>
2025-06-27 22:56:37 +0300

    codecrafters submit [skip ci]

--- src/streams/stderr.rs
+++ src/streams/stderr.rs
@@ -1,20 +1,33 @@
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 
 pub fn parse_stderr_redirect(args: &mut Vec<String>) -> Option<File> {
     let args_clone = args.clone();
 
-    match args_clone.iter().position(|arg| arg == "2>") {
+    match args_clone
+        .iter()
+        .position(|arg| arg == "2>" || arg == "2>>")
+    {
         Some(index) => {
+            let operator = args_clone.get(index).unwrap();
+            let should_append = operator == "2>>";
             args.remove(index);
 
             match args_clone.get(index + 1) {
                 Some(file) => {
                     args.remove(index);
-                    Some(File::create(file).unwrap())
-                },
-                None => panic!("Invalid stderr redirect: output target missing")
+
+                    Some(
+                        OpenOptions::new()
+                            .write(true)
+                            .append(should_append)
+                            .create(true)
+                            .open(file)
+                            .unwrap(),
+                    )
+                }
+                None => panic!("Invalid stderr redirect: output target missing"),
             }
-        },
-        None => None
+        }
+        None => None,
     }
 }
--- src/streams/stdout.rs
+++ src/streams/stdout.rs
@@ -1,20 +1,33 @@
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 
 pub fn parse_stdout_redirect(args: &mut Vec<String>) -> Option<File> {
     let args_clone = args.clone();
 
-    match args_clone.iter().position(|arg| arg == ">" || arg == "1>") {
+    match args_clone
+        .iter()
+        .position(|arg| arg == ">" || arg == "1>" || arg == ">>" || arg == "1>>")
+    {
         Some(index) => {
+            let operator = args_clone.get(index).unwrap();
+            let should_append = operator == ">>" || operator == "1>>";
             args.remove(index);
 
             match args_clone.get(index + 1) {
                 Some(file) => {
                     args.remove(index);
-                    Some(File::create(file).unwrap())
-                },
-                None => panic!("Invalid stdout redirect: output target missing")
+
+                    Some(
+                        OpenOptions::new()
+                            .write(true)
+                            .append(should_append)
+                            .create(true)
+                            .open(file)
+                            .unwrap(),
+                    )
+                }
+                None => panic!("Invalid stdout redirect: output target missing"),
             }
-        },
-        None => None
+        }
+        None => None,
     }
 }
