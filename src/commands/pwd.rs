1a07a533f187fcd593a0a4b96bedb9cc59115f35
Nelu Platonov <nelu.platonov@gmail.com>
2025-06-27 22:26:57 +0300

    codecrafters submit [skip ci]

--- src/commands/pwd.rs
+++ src/commands/pwd.rs
@@ -1,8 +1,18 @@
+use std::fs::File;
+use std::io::{stdout, Write};
 
 #[inline(always)]
-pub fn handle_pwd() {
+pub fn handle_pwd(stdout_file: Option<File>) {
     match std::env::current_dir() {
-        Ok(path) => println!("{}", path.display()),
+        Ok(path) => match stdout_file {
+            None => {
+                writeln!(stdout(), "{}", path.display()).unwrap();
+            }
+
+            Some(mut file) => {
+                writeln!(file, "{}", path.display()).unwrap();
+            }
+        },
         Err(err) => eprintln!("{err}"),
     }
 }
--- src/commands/type_.rs
+++ src/commands/type_.rs
@@ -1,13 +1,29 @@
 use crate::commands::commands::Commands;
 use crate::util::files::find_in_path;
+use std::fs::File;
+use std::io::{stdout, Write};
 
+// TODO: refactor to use dyn Write (though can't with Stdio) or something else
 #[inline(always)]
-pub fn handle_type(args: &mut Vec<String>) {
+pub fn handle_type(args: &mut Vec<String>, stdout_file: Option<File>) {
     let to_check = Commands::from_str(args.pop().unwrap().as_str());
     match to_check {
         Commands::Other(file) => match find_in_path(&file) {
-            Some(file_path) => println!("{file} is {file_path}"),
-            None => println!("{file}: not found"),
+            Some(file_path) => match stdout_file {
+                None => {
+                    writeln!(stdout(), "{file} is {file_path}").unwrap();
+                }
+                Some(mut redirect_file) => {
+                    writeln!(redirect_file, "{file} is {file_path}").unwrap()
+                }
+            },
+
+            None => match stdout_file {
+                None => writeln!(stdout(), "{file}: not found").unwrap(),
+                Some(mut redirect_file) => {
+                    writeln!(redirect_file, "{file}: not found").unwrap();
+                }
+            },
         },
         _ => println!("{} is a shell builtin", to_check.as_str()),
     }
