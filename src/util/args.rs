3d3cb338a56da5702da7a161bf5f2e231f875a40
Nelu Platonov <nelu.platonov@gmail.com>
2025-06-27 19:15:07 +0300

    codecrafters submit [skip ci]

--- src/util/args.rs
+++ src/util/args.rs
@@ -1,39 +1,169 @@
+const DOUBLE_QUOTES_ESCAPE_CHARS: [char; 2] = ['\"', '\\'];
+const SINGLE_QUOTES_ESCAPE_CHARS: [char; 1] = ['\''];
+const OUTSIDE_QUOTES_ESCAPE_CHARS: [char; 4] = [' ', '\'', '\"', '\\'];
+
+// .·°՞(っ-ᯅ-ς)՞°·. (╥﹏╥) ಥ﹏ಥ ⠀⠀⠀
 pub fn parse_args(args: &str) -> Vec<String> {
-    let args = args.trim().replace("''", "").replace("\"\"", "");
+    let mut result = Vec::new();
+    let mut is_inside_double_quotes = false;
+    let mut is_inside_single_quotes = false;
+    let mut prev_char_was_backslash = false;
 
-    // TODO: doesn't actually handle escaping properly (despite passing the stage)
-    // ex:
-    // input    --> echo "some item" \"another item\"
-    // expected --> some item "another item"
-    // actual   --> "some item" "another item"
-    if contains_unescaped_double_quotes(&args) {
-        split_by_separator(args, "\"")
-    } else if contains_unescaped_single_quotes(&args) {
-        split_by_separator(args, "'")
-    } else if !args.contains("\\") {
-        split_by_separator(args, " ")
-    } else {
-        return args
-            .replace("\\", "")
-            .split(" ")
-            .map(|arg| arg.to_owned())
-            .collect();
+    let mut current_arg = String::new();
+    // note: chars aren't grapheme clusters, assuming input is ASCII
+    for char in args.chars() {
+        if char == '\n' {
+            continue;
+        }
+
+        if char == '\\' {
+            if prev_char_was_backslash || is_inside_single_quotes {
+                current_arg.push('\\');
+            }
+
+            prev_char_was_backslash = !prev_char_was_backslash;
+            continue;
+        }
+
+        if char == '"' && !prev_char_was_backslash && !is_inside_single_quotes {
+            is_inside_double_quotes = !is_inside_double_quotes;
+        }
+
+        if char == '\'' && !prev_char_was_backslash && !is_inside_double_quotes {
+            is_inside_single_quotes = !is_inside_single_quotes;
+        }
+
+        if prev_char_was_backslash {
+            if is_inside_double_quotes && !DOUBLE_QUOTES_ESCAPE_CHARS.contains(&char) {
+                current_arg.push('\\');
+                current_arg.push(char);
+            } else {
+                current_arg.push(char);
+            }
+
+            prev_char_was_backslash = false;
+        } else {
+            if is_arg_separating_space(
+                is_inside_double_quotes,
+                is_inside_single_quotes,
+                &current_arg,
+                char,
+            ) {
+                result.push(current_arg);
+                current_arg = String::new();
+                continue;
+            }
+
+            if char_doesnt_require_escaping(is_inside_double_quotes, is_inside_single_quotes, &char)
+            {
+                current_arg.push(char);
+            }
+        }
     }
+
+    if !current_arg.trim().is_empty() {
+        result.push(current_arg)
+    }
+
+    result
 }
 
 #[inline(always)]
-fn contains_unescaped_single_quotes(args: &str) -> bool {
-    args.contains("'") && !args.contains("\\\'")
+fn char_doesnt_require_escaping(
+    is_inside_double_quotes: bool,
+    is_inside_single_quotes: bool,
+    char: &char,
+) -> bool {
+    (is_inside_double_quotes && !DOUBLE_QUOTES_ESCAPE_CHARS.contains(&char))
+        || (is_inside_single_quotes && !SINGLE_QUOTES_ESCAPE_CHARS.contains(&char))
+        || !OUTSIDE_QUOTES_ESCAPE_CHARS.contains(&char)
 }
 
 #[inline(always)]
-fn contains_unescaped_double_quotes(args: &str) -> bool {
-    args.contains("\"") && !args.contains("\\\"")
+fn is_arg_separating_space(
+    is_inside_double_quotes: bool,
+    is_inside_single_quotes: bool,
+    current_arg: &String,
+    char: char,
+) -> bool {
+    char == ' '
+        && !is_inside_double_quotes
+        && !is_inside_single_quotes
+        && !current_arg.trim().is_empty()
 }
 
-fn split_by_separator(args: String, separator: &str) -> Vec<String> {
-    args.split(separator)
-        .filter(|arg| !arg.trim().is_empty())
-        .map(|arg| arg.trim().to_owned())
-        .collect()
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parses_unescaped_single_quotes() {
+        assert_eq!(vec!["some item"], parse_args("\'some item\'"));
+    }
+
+    #[test]
+    fn parses_unescaped_double_quotes() {
+        assert_eq!(vec!["some item"], parse_args("\"some item\""));
+    }
+
+    #[test]
+    fn parses_escaped_single_quotes() {
+        assert_eq!(vec!["\'some", "item\'"], parse_args("\\\'some item\\\'"));
+    }
+
+    #[test]
+    fn parses_escaped_double_quotes() {
+        assert_eq!(vec!["\"some", "item\""], parse_args("\\\"some item\\\""));
+    }
+
+    #[test]
+    fn parses_literal_backslashes_within_single_quotes_1() {
+        assert_eq!(vec!["some\\\\\\item"], parse_args("\'some\\\\\\item\'"));
+    }
+
+    #[test]
+    fn parses_literal_backslashes_within_single_quotes_2() {
+        assert_eq!(
+            vec!["some\\\"test\\\"item"],
+            parse_args("\'some\\\"test\\\"item\'")
+        );
+    }
+
+    #[test]
+    fn parses_escaped_backslashes_within_double_quotes() {
+        assert_eq!(vec!["some\\item"], parse_args("\"some\\\\item\""));
+    }
+
+    #[test]
+    fn parses_escaped_double_quotes_within_double_quotes() {
+        assert_eq!(vec!["some\"item"], parse_args("\"some\\\"item\""));
+    }
+
+    #[test]
+    fn parses_backslash_with_non_escaped_chars_within_double_quotes() {
+        assert_eq!(vec!["\\5\\o"], parse_args("\"\\5\\o\""));
+    }
+
+    #[test]
+    fn parses_backslash_with_non_escaped_chars_within_single_quotes() {
+        assert_eq!(vec!["\\5\\o"], parse_args("\'\\5\\o\'"));
+    }
+
+    #[test]
+    fn parses_backslash_with_non_escaped_chars_outside_of_quotes() {
+        assert_eq!(vec!["5o"], parse_args("\\5\\o"));
+    }
+
+    #[test]
+    fn parses_backslash_with_single_quote_within_double_quotes() {
+        assert_eq!(vec!["\\23\\'"], parse_args("\"\\23\\'\""));
+    }
+
+    #[test]
+    fn parses_escaped_and_unescaped_double_quotes() {
+        assert_eq!(
+            vec!["some item", "\"another", "item\""],
+            parse_args("\"some item\" \\\"another item\\\"")
+        );
+    }
 }
