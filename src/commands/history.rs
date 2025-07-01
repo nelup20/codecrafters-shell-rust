77315905b9391d5106712627e0c803ebb6c52368
Nelu Platonov <nelu.platonov@gmail.com>
2025-07-01 23:47:54 +0300

    codecrafters submit [skip ci]

--- src/commands/history.rs
+++ src/commands/history.rs
@@ -1,5 +1,17 @@
 use crate::commands::command::Command;
 
 #[inline(always)]
-pub fn handle_history(command: Command) {
+pub fn handle_history(history: &Vec<String>) {
+    for (i, line) in history.iter().enumerate() {
+        println!("{} {}", i + 1, line)
+    }
+}
+
+pub fn add_to_history(history: &mut Vec<String>, command: &Command) {
+    let mut line = String::new();
+    line.push_str(command.command_type.as_str());
+    line.push_str(" ");
+    line.push_str(&command.args.join(" "));
+    
+    history.push(line);
 }
--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,18 @@ use crate::commands::pwd::handle_pwd;
 use crate::commands::type_::handle_type;
 use crate::streams::stdin::get_input_from_raw_mode;
 use commands::builtin_commands::BuiltinCommands;
-use crate::commands::history::handle_history;
+use crate::commands::history::{add_to_history, handle_history};
 
 fn main() {
+    let mut command_history: Vec<String> = Vec::new();
+    
     loop {
         let input = get_input_from_raw_mode();
         let commands = Command::parse_commands(&input);
         
         for command in commands {
+            add_to_history(&mut command_history, &command);
+            
             match &command.command_type {
                 CommandType::External(_) => handle_external(command),
                 CommandType::Builtin(cmd) => match cmd {
@@ -28,7 +32,7 @@ fn main() {
                     BuiltinCommands::Exit => handle_exit(command),
                     BuiltinCommands::Type => handle_type(command),
                     BuiltinCommands::Cd => handle_cd(command),
-                    BuiltinCommands::History => handle_history(command),
+                    BuiltinCommands::History => handle_history(&command_history),
                 }
             }
         }
